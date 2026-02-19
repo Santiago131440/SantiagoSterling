f56f49db25c218bc322707f2f34aa9d5ad2ddf44
Santiago Sterling <98182135+Santiago131440@users.noreply.github.com>
2026-02-18 19:10:54 -0500

    Update proyectos20.js

--- proyectos20.js
+++ proyectos20.js
@@ -9,6 +9,7 @@
       { id: 's8', name: 'ProyectoApp', url: 'https://santiago131440.github.io/SantiagoSterling/proyectoApp.html' },
       { id: 's9', name: 'Video Landing Page', url: 'https://santiago131440.github.io/SantiagoSterling/EntornoPrueba.html' },
       { id: 's10', name: 'Tabla de Salarios', url: 'https://santiago131440.github.io/SantiagoSterling/Salarios.html' },
+      { id: 's11', name: 'Portafolio 2', url: 'https://santiago131440.github.io/SantiagoSterling/index20.html' },
     ];
 
     // Elementos
@@ -205,6 +206,7 @@
     // Note about CORS / X-Frame-Options:
     // - algunos sitios (ej: google, muchas apps) bloquean ser embebidos en iframe.
     // - si ves un mensaje de bloqueo, considera usar una URL clonada o una captura/preview generada del sitio.
+
 
 
 
